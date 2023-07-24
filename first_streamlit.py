098ef28cbb039560dfc3d34df38d7445a8200583
Fernando Guerreiro <100739527+fsguerreiro@users.noreply.github.com>
2023-07-24 19:41:20 -0300

    Update first_streamlit.py

--- first_streamlit.py
+++ first_streamlit.py
@@ -4,3 +4,8 @@ import numpy
 
 st.set_page_config(page_title='My first app')
 st.write('# My first page')
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
